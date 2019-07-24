40e195d3128a94e4f2f9c6fe2aa00d345e029797
Joaquín Vicente <joakin@gmail.com>
2019-07-24 03:42:21 -0300

    Delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
   end
 end
